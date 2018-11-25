9a838f9648e3fe7c18d61ade6851317acf8a4c3a
KimHunJin <ysg01129@naver.com>
2018-11-25 19:59:53 +0900

    시크릿 다이어리 프로젝트

---
+++ SecretDiary/app/src/main/java/com/dxmnd/secretdiary/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.dxmnd.secretdiary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import com.dxmnd.secretdiary.utils.decrypt
+import com.dxmnd.secretdiary.utils.encrypt
+import com.dxmnd.secretdiary.utils.log
+import com.dxmnd.secretdiary.utils.md5
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+// 키로 키 암호화 --> 저장
+// 암호화 된 키로 글 암호화
+
+// 키로 키 암호화
+// 암호화 된 키로 글 복호화
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val key = "Secret" // 사용자가 입력한 비밀키 (PK가 된다)
+
+        val hashKey = md5(key)
+
+        var hash = ""
+        for(i in 0 until  hashKey.length) {
+            if(hashKey[i] != '-') {
+                hash += hashKey[i]
+            }
+        }
+
+        log(hash)
+
+        val keyBytes = hash.toByteArray(charset("UTF-8"))
+        val ivBytes = byteArrayOf(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+
+        btn_encryption.setOnClickListener {
+            val enText = encrypt(ivBytes, keyBytes, txt_main.text.toString().toByteArray())
+            val text = Base64.encodeToString(enText, Base64.DEFAULT)
+            txt_main.text = text
+        }
+        btn_decryption.setOnClickListener {
+            val deText = decrypt(ivBytes, keyBytes, Base64.decode(txt_main.text.toString().toByteArray(),Base64.DEFAULT))
+            val text = String(deText)
+            txt_main.text = text
+        }
+    }
+}
--- SecretDiary/app/src/main/java/com/dxmnd/secretdiary/utils/Secret.kt
+++ SecretDiary/app/src/main/java/com/dxmnd/secretdiary/utils/Secret.kt
@@ -3,6 +3,7 @@ package com.dxmnd.secretdiary.utils
 import java.io.UnsupportedEncodingException
 import java.security.InvalidAlgorithmParameterException
 import java.security.InvalidKeyException
+import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.BadPaddingException
@@ -21,12 +22,14 @@ import javax.crypto.spec.SecretKeySpec
         IllegalBlockSizeException::class,
         BadPaddingException::class)
 fun encrypt(ivByte: ByteArray, keyByte: ByteArray, textByte: ByteArray): ByteArray {
-    var ivSpec:AlgorithmParameterSpec = IvParameterSpec(ivByte)
+    var ivSpec: AlgorithmParameterSpec = IvParameterSpec(ivByte)
     var newKey = SecretKeySpec(keyByte, "AES")
     var cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
     cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec)
     return cipher.doFinal(textByte)
 }
+
+
 
 @Throws(UnsupportedEncodingException::class,
         NoSuchAlgorithmException::class,
@@ -35,10 +38,30 @@ fun encrypt(ivByte: ByteArray, keyByte: ByteArray, textByte: ByteArray): ByteArr
         InvalidAlgorithmParameterException::class,
         IllegalBlockSizeException::class,
         BadPaddingException::class)
-fun decrypt(ivByte: ByteArray, keyByte: ByteArray, textByte: ByteArray) : ByteArray {
-    var ivSpec:AlgorithmParameterSpec = IvParameterSpec(ivByte)
+fun decrypt(ivByte: ByteArray, keyByte: ByteArray, textByte: ByteArray): ByteArray {
+    var ivSpec: AlgorithmParameterSpec = IvParameterSpec(ivByte)
     var newKey = SecretKeySpec(keyByte, "AES")
     var cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
     cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec)
     return cipher.doFinal(textByte)
+}
+
+
+
+@Throws(NoSuchAlgorithmException::class)
+fun md5(s: String): String {
+    val digest = MessageDigest.getInstance("MD5")
+    digest.update(s.toByteArray())
+    val messageDigest = digest.digest()
+
+    val stringBuffer = StringBuffer()
+
+    for (i in 0 until messageDigest.size) {
+        var h = messageDigest[i].toString(16)
+        while (h.length < 2) {
+            h = "0$h"
+        }
+        stringBuffer.append(h)
+    }
+    return stringBuffer.toString()
 }
